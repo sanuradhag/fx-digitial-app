e4de8ef8589d12593a39b07b50daee3cfa87000a
Anuradha Gunasekara <saggunasekara@gmail.com>
2022-09-01 19:01:26 +0100

    add filtering functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import ItemDetail from './pages/ItemDetail';
 
 import './global.css';
